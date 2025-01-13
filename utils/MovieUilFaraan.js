e7c3747d15fdf7e55b6e98d8958c2d0cab57ce29
FIRDAUS7777 <148176150+FIRDAUS7777@users.noreply.github.com>
2025-01-13 22:00:54 +0800

    Update MovieUilFaraan.js

--- utils/MovieUilFaraan.js
+++ utils/MovieUilFaraan.js
@@ -1,74 +1,81 @@
 const { Movie } = require('../models/Movie');
 const fs = require('fs').promises;
 
+// Helper function to read the JSON file
 async function readJSON(filename) {
     try {
         const data = await fs.readFile(filename, 'utf8');
         return JSON.parse(data);
-    } catch (err) { console.error(err); throw err; }
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
 }
 
+// Helper function to write data to the JSON file
 async function writeJSON(object, filename) {
     try {
         const allObjects = await readJSON(filename);
         allObjects.push(object);
-
         await fs.writeFile(filename, JSON.stringify(allObjects), 'utf8');
         return allObjects;
-    } catch (err) { console.error(err); throw err; }
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
 }
 
+// Edit movie function
 async function editMovie(req, res) {
     try {
-        const id = req.params.id;
-        const movieImage = req.body.movieImage;
-        const movieTitle = req.body.movieTitle;
-        const moviedescription = req.body.movieDescription;
-        const moviedirectors = req.body.movieDirectors;
-        const moviewriters = req.body.movieWriters;
-        const moviestars = req.body.movieStars;
+        const { id } = req.params;
+        const { movieImage, movieTitle, movieDescription, movieDirectors, movieWriters, movieStars } = req.body;
 
+        // Validation for movieTitle
+        if (!movieTitle) {
+            return res.status(400).json({ message: 'The movie title must be filled' });
+        }
+        if (typeof movieTitle !== 'string') {
+            return res.status(400).json({ message: 'The movie title must be a string' });
+        }
+        if (movieTitle.trim().length === 0) {
+            return res.status(400).json({ message: 'The movie title must not be empty' });
+        }
+
+        // Read existing movies data from JSON file
         const allMovies = await readJSON('utils/movies.json');
 
-        var modified = false;
-
-        for (var i = 0; i < allMovies.length; i++) {
-    var curcurrResource = allMovies[i];
-    if (curcurrResource.id == id) {
-        allMovies[i].movieImage = movieImage;
-        allMovies[i].movieTitle = movieTitle;
-        allMovies[i].movieDescription = moviedescription;
-        allMovies[i].movieDirectors = moviedirectors;
-        allMovies[i].movieWriters = moviewriters;
-        allMovies[i].movieStars = moviestars;
-        modified = true;
-    }
-}
-
+        // Find and modify the movie
+        let modified = false;
+        for (let i = 0; i < allMovies.length; i++) {
+            if (allMovies[i].id === id) {
+                allMovies[i].movieImage = movieImage || allMovies[i].movieImage;
+                allMovies[i].movieTitle = movieTitle;
+                allMovies[i].movieDescription = movieDescription || allMovies[i].movieDescription;
+                allMovies[i].movieDirectors = movieDirectors || allMovies[i].movieDirectors;
+                allMovies[i].movieWriters = movieWriters || allMovies[i].movieWriters;
+                allMovies[i].movieStars = movieStars || allMovies[i].movieStars;
+                modified = true;
+                break;
+            }
         }
 
-        if (!movieTitle) {
-            return res.status(500).json({ message: 'The movie title must be filled' });
-        }
-        else if (typeof movieTitle !== 'string') {
-            return res.status(500).json({ message: 'The movie title must be a string' });
-        }
-        else if (movieTitle.trim().length === 0) {
-            return res.status(500).json({ message: 'The movie title must not be empty' });
-        }
-        else if (modified) {
+        // If movie is modified, save the updated data
+        if (modified) {
             await fs.writeFile('utils/movies.json', JSON.stringify(allMovies), 'utf8');
-            return res.status(201).json({ message: 'Movie modified successfully!' });
+            return res.status(200).json({ message: 'Movie modified successfully!' });
         } else {
-            return res.status(500).json({ message: 'Error occurred, unable to modify!' });
+            return res.status(404).json({ message: 'Movie not found' });
         }
     } catch (error) {
-        return res.status(500).json({ message: error.message });
+        console.error(error);
+        return res.status(500).json({ message: 'An error occurred while modifying the movie' });
     }
 }
 
-
-
 module.exports = {
-    readJSON, writeJSON, editMovie
+    readJSON, 
+    writeJSON, 
+    editMovie
 };
+
