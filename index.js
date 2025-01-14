c4999cfa8ff74e446a2d9292f81b55b9b9dcdf96
FIRDAUS7777 <148176150+FIRDAUS7777@users.noreply.github.com>
2025-01-14 10:57:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
-var express = require('express');
-var bodyParser = require("body-parser");
-var app = express();
+// Import required modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path'); // For resolving file paths
 
-const PORT = process.env.PORT || 5050
-var startPage = "index.html";
+const app = express();
+const PORT = process.env.PORT || 5050;
+const startPage = 'index.html';
 
+// Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static("./public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/public/" + startPage);
-})
-
-const { addMovie} = require('./utils/MovieUilAlz');
-app.post('/add-resource', addMovie);
-
-const {  editMovie } = require('./utils/MovieUilFaraan');
-app.put('/edit-movie/:id', editMovie);
-
-const { viewResources } = require('./utils/MovieUilFir');
-app.get('/view-resource/:search', viewResources);
-
-
-server = app.listen(PORT, function () {
-    const address = server.address();
-    const baseUrl = `http://${address.address == "::" ? 'localhost' : address.address}:${address.port}`;
-    console.log(`Demo project at: ${baseUrl}`);
+    res.sendFile(path.join(__dirname, 'public', startPage));
 });
 
-module.exports = {app, server,}
+// Import route handlers from utils
+const { addMovie } = require('./utils/MovieUilAlz');
+const { editMovie } = require('./utils/MovieUilFaraan');
+const { viewResources } = require('./utils/MovieUilFir');
+
+// API endpoints
+app.post('/add-resource', addMovie);
+app.put('/edit-movie/:id', editMovie);
+app.get('/view-resource/:search', viewResources);
+
+// Start the server
+const server = app.listen(PORT, () => {
+    const address = server.address();
+    const baseUrl = `http://${address.address === '::' ? 'localhost' : address.address}:${address.port}`;
+    console.log(`Demo project running at: ${baseUrl}`);
+});
+
+// Export app and server for testing
+module.exports = { app, server };
